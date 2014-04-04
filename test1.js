d790a8fd5ed5208086bb5e2395e367602ca56bbd
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-04-04 16:30:36 +0100

    WIP

--- test1.js
+++ test1.js
@@ -5,11 +5,15 @@ var argv = require("optimist")
 		.argv,
 	_ = require('underscore');
 
+/*
 var monitors = { };
-
 _.each([ 'EUS' ], function (stationCode) {
 	monitors[stationCode] = new require('./arrivalsMonitor')(stationCode, argv.out);	
 	monitors[stationCode].onArrival(function (stationCode, train) {
 		console.log(stationCode + ": arrival of " + JSON.stringify(train));
 	})
 });
+*/
+
+var trainsMonitor = require('./trainsMonitor')(argv.out);
+trainsMonitor.add('BSH', 'EUS', new Date(), function (err) { });	
