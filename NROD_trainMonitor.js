f23be2983e5cbcba20835a25a233970ae6a07e9b
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-04-13 20:15:59 +0100

    Good progress monitoring arrivals

--- NROD_trainMonitor.js
+++ NROD_trainMonitor.js
@@ -38,25 +38,42 @@ client.connect(function (sessionId) {
                     if (tiploc) {
                         event = event.body;
                         event.loc_tiploc = tiploc;
-                        // NOTE: I presume that where gbtt_timestamp is not defined, it 
-                        // is the same as planned_timestamp
-                        event.gbtt_timestamp = event.gbtt_timestamp || event.planned_timestamp;
-                        // I correct all dates to remove the BST correction
-
-                        memo.push(event);
+                        // TODO: the timestamps in the TRAIN_MVT_ALL_TOC feed  
+                        // are adjusted for BST! I need to bring them back to 
+                        // GMT
+                        [ 'original_loc_timestamp', 'gbtt_timestamp', 'planned_timestamp', 'actual_timestamp' ].forEach(function (propertyName) {
+                            if (event[propertyName]) event[propertyName] = new Date(parseInt(event[propertyName]) - 60 * 60000);
+                        });
+                        // NOTE: 
+                        // a) I presume that where gbtt_timestamp is not 
+                        //    defined, it is the same as planned_timestamp, 
+                        //    rounded to the next minute if necessary
+                        // b) there are trains that have no planned_timestamp
+                        //    anyway! I just give up on them
+                        if (!event.gbtt_timestamp && event.planned_timestamp) {
+                            event.gbtt_timestamp = event.planned_timestamp;
+                            if (event.gbtt_timestamp.getSeconds() > 0) {
+                                event.gbtt_timestamp.setMinutes(event.gbtt_timestamp.getMinutes() + 1);
+                                event.gbtt_timestamp.setSeconds(0);
+                            }
+                        }                        
+                        if (event.gbtt_timestamp) memo.push(event);
                     }
                     callback(null, memo);
                 });
             }
         }, function (err, arrivals) {
             arrivals.forEach(function (arrival) {
-                utils.log(arrival.loc_tiploc + ": arrival of service " + arrival.train_service_code + " at " + arrival.actual_timestamp + " rather than " + arrival.gbtt_timestamp);
-                // TODO: the timestamps in the TRAIN_MVT_ALL_TOC feed is 
-                // adjusted for BST! I need to bring it back to GMT
-                var trainKey = arrival.loc_tiploc + '_' + parseInt(arrival.gbtt_timestamp + 24 * 60 * 60000);
-                if (monitoredTrains[trainKey]) {
-                    // call all the callbacks!
+
+                function prettyPrint (d) {
+                    return (d.getDate() < 10 ? '0' : '') + d.getDate() + " " + (d.getHours() < 10 ? '0' : '') + d.getHours() + ":" + (d.getMinutes() < 10 ? '0' : '') + d.getMinutes() + ":" + (d.getSeconds() < 10 ? '0' : '') + d.getSeconds();                   
                 }
+
+                if (arrival.loc_tiploc === 'EUSTON') utils.log(arrival.loc_tiploc + ": arrival of service " + arrival.train_service_code + " at " + prettyPrint(arrival.actual_timestamp) + " rather than " + prettyPrint(arrival.gbtt_timestamp));
+                // var trainKey = arrival.loc_tiploc + '_' + arrival.gbtt_timestamp.getTime();
+                // if (monitoredTrains[trainKey]) {
+                //     // call all the callbacks!
+                // }
             });
         });
     });
