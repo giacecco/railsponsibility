d0eddf4cea10e9b2b062cacc39d8ad49306735f9
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-04-13 11:57:12 +0100

    Added tiploc2stanox to utils

--- NROD_arrivalsMonitor.js
+++ NROD_arrivalsMonitor.js
@@ -1,32 +1,23 @@
 var fs = require('fs'),
     Stomp = require('stomp-client'),
-    path = require('path'),
+    utils = require('./utils'),
 	_ = require('underscore');
 
 var SECRET = JSON.parse(fs.readFileSync(path.join(__dirname, 'NROD_SECRET.json'))),
     STATION_CODES = null,
     client = new Stomp('datafeeds.networkrail.co.uk', 61618, SECRET.username, SECRET.password);
 
-function initialise (callback) {
-    if (STATION_CODES) return callback(null); 
-    csv()
-        .from.path(path.join(__dirname, "railwaycodes_org_uk.csv"), {
-            columns: true
-        })  
-        .to.array(function (stationCodes) {
-            STATION_CODES = stationCodes;
-            callback(null);
-        });
-}
-
-initialise(function (err) {
-    client.connect(function (sessionId) {
-        client.subscribe('/topic/TRAIN_MVT_ALL_TOC', function (events, headers) {
-            events = _.map(JSON.parse(events), function (event) { return event.body; });
-            _.each(_.filter(events, function (event) { return event.event_type === 'ARRIVAL'; }), function (event) {
-                console.log('****************');
-                console.log(event);
-            });
+/*
+client.connect(function (sessionId) {
+    client.subscribe('/topic/TRAIN_MVT_ALL_TOC', function (events, headers) {
+        events = _.map(JSON.parse(events), function (event) { return event.body; });
+        _.each(_.filter(events, function (event) { return event.event_type === 'ARRIVAL'; }), function (event) {
+            console.log('****************');
+            console.log(event);
         });
     });
+});
+*/
+utils.tiploc2stanox('BERKHMD', function (err, stanox) {
+    console.log(stanox);
 });
--- utils.js
+++ utils.js
@@ -1,3 +1,10 @@
+var AsyncCache = require('async-cache')
+	csv = require('csv'),
+	path = require('path');
+
+var STATION_CODES_CONVERSION_CACHE_SIZE = 100, // TODO: does this number make sense?
+	STATION_CODES = null;
+
 exports.dateToCSVDate = function (d) {
 	return d.getFullYear() + "/" + (d.getMonth() < 9 ? '0' : '') + (d.getMonth() + 1) + "/" + (d.getDate() < 10 ? '0' : '') + d.getDate() + " " + (d.getHours() < 10 ? '0' : '') + d.getHours() + ":" + (d.getMinutes() < 10 ? '0' : '') + d.getMinutes() + ":" + (d.getSeconds() < 10 ? '0' : '') + d.getSeconds();
 }
@@ -6,3 +13,36 @@ exports.log = function (s) {
 	console.log(exports.dateToCSVDate(new Date()) + " - " + s);
 }
 
+var stationCodesInitialiseCached = new AsyncCache({ 
+	'maxAge': 24 * 60 * 60000, // 1 day 
+	'load': function (key, callback) {
+	    csv()
+	        .from.path(path.join(__dirname, "railwaycodes_org_uk.csv"), {
+	            columns: true
+	        })  
+	        .to.array(function (stationCodes) {
+	        	STATION_CODES = stationCodes
+	            callback(null, null);
+	        });
+	}
+});
+
+function stationCodesInitialise (callback) {
+	stationCodesInitialiseCached.get(null, callback);
+}
+
+var tiploc2stanoxCached = new AsyncCache({
+	'max': STATION_CODES_CONVERSION_CACHE_SIZE, 
+	'load': function (key, callback) {
+		stationCodesInitialise(function (err) {
+			var station = STATION_CODES.filter(function (sc) {
+				return sc.tiploc === key;
+			})[0];
+			callback(null, station ? station.stanox : null);
+		});
+	}
+});
+
+exports.tiploc2stanox = function (tiploc, callback) {
+	tiploc2stanoxCached.get(tiploc.toUpperCase(), callback);
+};
