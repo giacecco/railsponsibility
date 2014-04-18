04810f09c9902f09cf0cdf79620bffdd323ad2a6
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-04-18 15:36:57 +0100

    the original, simple version is back

---
+++ utils.js
@@ -0,0 +1,7 @@
+exports.dateToCSVDate = function (d) {
+	return d.getFullYear() + "/" + (d.getMonth() < 9 ? '0' : '') + (d.getMonth() + 1) + "/" + (d.getDate() < 10 ? '0' : '') + d.getDate() + " " + (d.getHours() < 10 ? '0' : '') + d.getHours() + ":" + (d.getMinutes() < 10 ? '0' : '') + d.getMinutes() + ":" + (d.getSeconds() < 10 ? '0' : '') + d.getSeconds();
+}
+
+exports.log = function (s) {
+	if (process.env.NODEJITSU !== "TRUE") console.log(exports.dateToCSVDate(new Date()) + " - " + s);
+}
