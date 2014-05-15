6a684323d98961b7a22acec5e7a8511659f10beb
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-05-15 12:37:45 +0100

    Early index for the S3 logs repository

---
+++ S3/index.js
@@ -0,0 +1,4 @@
+var generateFilename = function () {
+    var d = new Date();
+    return 'arrivals_' + d.getFullYear() + (d.getMonth() < 9 ? '0' : '') + (d.getMonth() + 1) + (d.getDate() < 10 ? '0' : '') + d.getDate() + (d.getHours() < 10 ? '0' : '') + d.getHours() + '.csv';
+};
